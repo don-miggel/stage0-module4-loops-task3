fee832071edd99230f3bd806565bf1d54395d453
Mykhailo Derecha <don.miggel@yahoo.com>
2023-02-25 21:33:05 +0200

    Module 4.2, task 3 (For loop) modified v.0.3

--- src/main/java/school/mjc/stage0/loops/task3/GreatestCommonDivisor.java
+++ src/main/java/school/mjc/stage0/loops/task3/GreatestCommonDivisor.java
@@ -2,16 +2,20 @@ package school.mjc.stage0.loops.task3;
 
 public class GreatestCommonDivisor {
     public void printGCD(int first, int second) {
+        int dividorFinal=0;
         if (first > second)
-            for (int dividor = 1; dividor < first; dividor++){
+            for (int dividor = 1; dividor <= first; dividor++){
                 if (first % dividor == 0 && second % dividor == 0 && dividor > 1)
-                    System.out.println(dividor);
+//                    System.out.println(dividor);
+                    dividorFinal = dividor;
             }
         else{
-            for (int dividor = 1; dividor < second; dividor++){
+            for (int dividor = 1; dividor <= second; dividor++){
                 if (first % dividor == 0 && second % dividor == 0 && dividor > 1)
-                    System.out.println(dividor);
+//                    System.out.println(dividor);
+                    dividorFinal = dividor;
             }
         }
+        System.out.println(dividorFinal);
     }
 }
