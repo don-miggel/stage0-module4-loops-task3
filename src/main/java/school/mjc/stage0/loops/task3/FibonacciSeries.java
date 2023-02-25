62077d41fc05fb4eb3414162416db21ad2500f49
Mykhailo Derecha <don.miggel@yahoo.com>
2023-02-25 21:24:19 +0200

    Module 4.2, task 3 (For loop) modified v.0.1

--- src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
+++ src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
@@ -4,16 +4,19 @@ public class FibonacciSeries {
     public void printFibonacci(int lastFibonacci) {
         int prior = 0;
         int next = 1;
+        int res =0;
+        if (lastFibonacci == 0)
+            System.out.println(res);
         System.out.println(prior);
-        for(int i = 2; i <= lastFibonacci;i++){
+        for(int i = 2; i <= lastFibonacci; i++){
 
             System.out.println(next);
 //            System.out.println(next+"nx");
 //            System.out.println(res);
-//            res += next;
-//            prior = next;
-            next += prior;
+            res = (prior+next);
             prior = next;
+//            next += prior;
+            next = res;
 
         }
 
