b423063e285b063466697bbe0d17990b86d3dc05
Mykhailo Derecha <don.miggel@yahoo.com>
2023-02-25 21:28:46 +0200

    Module 4.2, task 3 (For loop) modified v.0.2

--- src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
@@ -2,7 +2,7 @@ package school.mjc.stage0.loops.task3;
 
 public class DigitsSum {
     public void printDigitsSum(int t){
-        String numAsStr = Integer.toString(t);
+        String numAsStr = Integer.toString(Math.abs(t));
         int sum = 0;
         for(int i = 0; i < numAsStr.length(); i++)
             sum+= Integer.parseInt(String.valueOf(numAsStr.charAt(i)));
